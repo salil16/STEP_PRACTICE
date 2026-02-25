e93357062d3dbdeee8270220c85af62652053061
salil <abhiram.builds@gmail.com>
2026-02-25 11:13:12 +0530

    UC5: Palindrome using Stack

---
+++ src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome");
+        } else {
+            System.out.println("The given string is NOT a Palindrome");
+        }
+    }
+}
